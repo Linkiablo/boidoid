b6eab15bbb2a79c834dea8ee81e7f1fe62babb0a
Linkiablo <johajansen@googlemail.com>
2023-12-25 13:15:50 +0100

    added config

--- main.go
+++ main.go
@@ -8,19 +8,15 @@ import (
 	"golang.org/x/exp/rand"
 )
 
-// TODO: config interactive
-// TODO: find a more general configuration, independent of terminal size
-const SEPARATIONFACTOR = 0.05
-const ALIGNMENTFACTOR = 0.05
-const COHESIONFACTOR = 0.0008
-const TURNIMPULSE = 0.2
-const MARGIN = 25
-const SEPARATIONTHRESHOLD = 2
-const MAXSPEED = 2.5
-const MINSPEED = 1
-
-func clamp(val, min, max float64) float64 {
-	return math.Min(math.Max(val, min), max)
+type config struct {
+	separationFactor    float64
+	alignmentFactor     float64
+	cohesionFactor      float64
+	turnImpulse         float64
+	margin              float64
+	separationThreshold float64
+	maxSpeed            float64
+	minSpeed            float64
 }
 
 type vec2 struct {
@@ -57,86 +53,18 @@ type birdoid struct {
 	flock   []*birdoid
 }
 
-func (b *birdoid) update(maxY, maxX int) {
-	invClose := vec2{0, 0}
-	avgHeading := vec2{0, 0}
-	avgPosition := vec2{0, 0}
-
-	for i := 0; i < len(b.flock); i++ {
-		if (b.pos.distance(b.flock[i].pos)) < SEPARATIONTHRESHOLD {
-			// neighbor direction for separation
-			invClose.x += b.pos.x - b.flock[i].pos.x
-			invClose.y += b.pos.y - b.flock[i].pos.y
-		}
-
-		// neighbor position to calculate cohesion
-		avgPosition.x += b.flock[i].pos.x
-		avgPosition.y += b.flock[i].pos.y
-
-		// avg neighbor heading for alignment
-		avgHeading.x += b.flock[i].moveDir.x
-		avgHeading.y += b.flock[i].moveDir.y
-
-	}
-
-	invClose.multiplyScalar(SEPARATIONFACTOR)
-
-	if len(b.flock) > 0 {
-		avgHeading.divideScalar(float64(len(b.flock)))
-		avgHeading.multiplyScalar(ALIGNMENTFACTOR)
-
-		avgPosition.divideScalar(float64(len(b.flock)))
-		// get movement vector by subtracting current position from avg neighbor position
-		avgPosition.subtract(b.pos)
-		avgPosition.multiplyScalar(COHESIONFACTOR)
-	}
-
-	b.moveDir.add(avgHeading)
-	b.moveDir.add(avgPosition)
-	b.moveDir.add(invClose)
-
-	// logFd, _ := os.OpenFile("log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	// defer logFd.Close()
-	// fmt.Fprintln(logFd, b)
-
-	if b.pos.x < MARGIN {
-		b.moveDir.x += TURNIMPULSE
-	}
-	if b.pos.x > float64(maxX-MARGIN) {
-		b.moveDir.x -= TURNIMPULSE
-	}
-	if b.pos.y < MARGIN {
-		b.moveDir.y += TURNIMPULSE
-	}
-	if b.pos.y > float64(maxY-MARGIN) {
-		b.moveDir.y -= TURNIMPULSE
-	}
-
-	speed := b.moveDir.distance(vec2{0, 0})
-	if speed > MAXSPEED {
-		b.moveDir.divideScalar(speed)
-		b.moveDir.multiplyScalar(MAXSPEED)
-	} else if speed < MINSPEED {
-		b.moveDir.divideScalar(speed)
-		b.moveDir.multiplyScalar(MINSPEED)
-	}
-
-	// finally update position
-	b.pos.add(b.moveDir)
-}
-
 type goggle struct {
+	config   config
 	radius   float64
 	birdoids []birdoid
 }
 
-func newGoggle(radius float64, numBirdoids, maxY, maxX int) goggle {
+func newGoggle(radius float64, numBirdoids, maxY, maxX int, conf config) goggle {
 	var bs []birdoid
 	for i := 0; i < numBirdoids; i++ {
-		// for now spawn in square area to avoid horizontal movement
 		pos := vec2{rand.Float64() * float64(maxY),
 			float64(maxX)/2 - float64(maxY)/2 + rand.Float64()*float64(maxY)}
-		moveDir := vec2{rand.Float64() * MINSPEED, rand.Float64() * MINSPEED}
+		moveDir := vec2{rand.Float64() * conf.minSpeed, rand.Float64() * conf.minSpeed}
 
 		bs = append(bs, birdoid{
 			pos:     pos,
@@ -145,6 +73,7 @@ func newGoggle(radius float64, numBirdoids, maxY, maxX int) goggle {
 	}
 
 	return goggle{
+		config:   conf,
 		radius:   radius,
 		birdoids: bs,
 	}
@@ -167,7 +96,68 @@ func (g *goggle) updateFlocks(maxY, maxX int) {
 	}
 
 	for i := 0; i < len(g.birdoids); i++ {
-		g.birdoids[i].update(maxY, maxX)
+		b := &g.birdoids[i]
+		invClose := vec2{0, 0}
+		avgHeading := vec2{0, 0}
+		avgPosition := vec2{0, 0}
+
+		for i := 0; i < len(b.flock); i++ {
+			if (b.pos.distance(b.flock[i].pos)) < g.config.separationThreshold {
+				// neighbor direction for separation
+				invClose.x += b.pos.x - b.flock[i].pos.x
+				invClose.y += b.pos.y - b.flock[i].pos.y
+			}
+
+			// neighbor position to calculate cohesion
+			avgPosition.x += b.flock[i].pos.x
+			avgPosition.y += b.flock[i].pos.y
+
+			// avg neighbor heading for alignment
+			avgHeading.x += b.flock[i].moveDir.x
+			avgHeading.y += b.flock[i].moveDir.y
+
+		}
+
+		invClose.multiplyScalar(g.config.separationFactor)
+
+		if len(b.flock) > 0 {
+			avgHeading.divideScalar(float64(len(b.flock)))
+			avgHeading.multiplyScalar(g.config.alignmentFactor)
+
+			avgPosition.divideScalar(float64(len(b.flock)))
+			// get movement vector by subtracting current position from avg neighbor position
+			avgPosition.subtract(b.pos)
+			avgPosition.multiplyScalar(g.config.cohesionFactor)
+		}
+
+		b.moveDir.add(avgHeading)
+		b.moveDir.add(avgPosition)
+		b.moveDir.add(invClose)
+
+		if b.pos.x < g.config.margin {
+			b.moveDir.x += g.config.turnImpulse
+		}
+		if b.pos.x > float64(maxX)-g.config.margin {
+			b.moveDir.x -= g.config.turnImpulse
+		}
+		if b.pos.y < g.config.margin {
+			b.moveDir.y += g.config.turnImpulse
+		}
+		if b.pos.y > float64(maxY)-g.config.margin {
+			b.moveDir.y -= g.config.turnImpulse
+		}
+
+		speed := b.moveDir.distance(vec2{0, 0})
+		if speed > g.config.maxSpeed {
+			b.moveDir.divideScalar(speed)
+			b.moveDir.multiplyScalar(g.config.maxSpeed)
+		} else if speed < g.config.minSpeed {
+			b.moveDir.divideScalar(speed)
+			b.moveDir.multiplyScalar(g.config.minSpeed)
+		}
+
+		// finally update position
+		b.pos.add(b.moveDir)
 	}
 }
 
@@ -191,7 +181,18 @@ func main() {
 	goncurses.HalfDelay(1)
 
 	maxY, maxX := win.MaxYX()
-	g := newGoggle(20, 250, maxY, maxX)
+	conf := config{
+		separationFactor:    0.05,
+		alignmentFactor:     0.05,
+		cohesionFactor:      0.0008,
+		turnImpulse:         0.2,
+		margin:              25,
+		separationThreshold: 2,
+		maxSpeed:            2.5,
+		minSpeed:            1,
+	}
+	g := newGoggle(20, 25, maxY, maxX, conf)
+
 	for {
 		maxY, maxX = win.MaxYX()
 		draw(&g, win)
